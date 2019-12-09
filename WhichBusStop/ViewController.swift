3ba01390a79ce7159f2de1a5cb45eea70fb3835b
hadjioul <leila.hadjioual@gmail.com>
2019-12-09 10:03:02 +0100

    add itiniraire

--- WhichBusStop/ViewController.swift
+++ WhichBusStop/ViewController.swift
@@ -11,16 +11,20 @@ import MapKit
 import CoreLocation
 import Contacts
 
-class ViewController: UIViewController, CLLocationManagerDelegate{
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate{
 
     @IBOutlet weak var mapView: MKMapView!
     let locationManager = CLLocationManager()
     let regionInMeters: Double = 100
+   let sourcePoint = CLLocationCoordinate2D(latitude: 45.191302, longitude: 5.715173)
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         checkLocationServices()
+        //let sourcePoint = CLLocationCoordinate2D(latitude: 45.191302, longitude: 5.715173)
+        //let destinationPoint = CLLocationCoordinate2D(latitude: 45.191587, longitude: 5.714554)
+        //directionsRequest(source: sourcePoint, destination: destinationPoint)
        
     }
     
@@ -80,12 +84,14 @@ class ViewController: UIViewController, CLLocationManagerDelegate{
         }
     }
     
-//update the position of the user when he move
+//update the position of the user when he move and show buses points with itineraire
     func locationManager(_ manager:CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.last else{return}
         let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
         let region = MKCoordinateRegion.init(center: center, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
         mapView.setRegion(region, animated:true)
+       
+      
         let api = Api()
         var stopPoints: [Stop]?
         api.getStopPoint(longitude: center.longitude, latitude: center.latitude) { (stops) in
@@ -95,16 +101,54 @@ class ViewController: UIViewController, CLLocationManagerDelegate{
                 let coordinates2D = CLLocationCoordinate2D(latitude: stop.lat!, longitude: stop.lon!)
                 let place = MKPlacemark(coordinate: coordinates2D)
                 self.mapView.addAnnotation(place)
+                
+                    
+                
+               
             })
             
-       
         }
-        
     }
     
     //verify differents permissions
     func locationManager(_ manager:CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus){
         checkLocationAuthorization()
     }
+    
+    
+    //create itineraire
+    func directionsRequest(source: CLLocationCoordinate2D, destination: CLLocationCoordinate2D) {
+        let request = MKDirections.Request()
+        
+            request.source = MKMapItem(placemark: MKPlacemark(coordinate: source, addressDictionary: nil))
+            request.destination = MKMapItem(placemark: MKPlacemark(coordinate: destination, addressDictionary: nil))
+            request.requestsAlternateRoutes = true
+            request.transportType = .walking
+
+            let directions = MKDirections(request: request)
+
+            directions.calculate { [unowned self] response, error in
+                guard let unwrappedResponse = response else { return }
+
+                for route in unwrappedResponse.routes {
+                    self.mapView.addOverlay(route.polyline)
+                    self.mapView.setVisibleMapRect(route.polyline.boundingMapRect, animated: true)
+                    }
+                self.mapView.delegate = self
+            }
+    }
+    
+   
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKPolylineRenderer(polyline: overlay as! MKPolyline)
+        renderer.strokeColor = UIColor.blue
+        renderer.lineWidth = 4.0
+        return renderer
+    }
+    
+    
+
+    
 }
 
